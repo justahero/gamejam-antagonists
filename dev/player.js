9e6717b29db971e252e01c4bce74c06a7c967d01
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-04-29 18:06:17 +0200

    Add two players

--- dev/player.js
+++ dev/player.js
@@ -1,4 +1,18 @@
-var Player = function(name, color) {
-  this.name  = name;
-  this.color = color;
+var Player = function(name, color, posx, posy, width, height) {
+  this.name   = name;
+  this.x      = posx;
+  this.y      = posy;
+  this.xSpeed = 0;
+  this.ySpeed = 0;
+
+  this.update = function(){
+    this.x += this.xSpeed * width;
+    this.y += this.ySpeed * height;
+  }
+
+  this.show = function(){
+    fill(255, 0, 0);
+    rect(this.x, this.y, width, height);
+    // fill(255);
+  }
 }
--- dev/sketch.js
+++ dev/sketch.js
@@ -7,8 +7,9 @@ var board_rows = 30;
 var cell_width  = window_width / board_cols;
 var cell_height = window_height / board_rows;
 
-var board = new Board(board_cols, board_rows, cell_width, cell_height);
-let players = []
+let board = new Board(board_cols, board_rows, cell_width, cell_height);
+let p1;
+let p2;
 
 function setup() {
   createCanvas(window_width, window_height);
@@ -16,27 +17,61 @@ function setup() {
   noStroke();
   loop();
 
+  p1 = new Player("Green", color(255, 0, 0), 10 * cell_width, 10 * cell_height, cell_width, cell_height); 
+  p2 = new Player("Red", color(0, 255, 0), 5 * cell_width, 5 * cell_height, cell_width, cell_height); 
+  console.log(p1);
+
   // set up boards / players
-  initialize();
   generateBoard(board);
+  initialize();
 }
 
 function initialize() {
-  players.push(new Player("Green", color(255, 0, 0))),
-  players.push(new Player("Red", color(0, 255, 0))),
-
-  board.addPlayer(players[0], 0, 0);
-  board.addPlayer(players[1], 20, 20);
+  board.addPlayer(p1, 0, 0);
+  board.addPlayer(p2, 29, 29);
 }
 
 function draw() {
   clear();
   background(224);
 
-  updateInput();
   updateAnimation();
 
   renderGrid();
+
+  if (p1 != undefined) {
+    p1.show();
+    p1.update();
+    if (p1.x > window_width) {
+      p1.x = window_width - cell_width;
+    }
+    if (p1.y > window_height) {
+      p1.y = window_height - cell_height;
+    }
+    if (p1.x < 0) {
+      p1.x = 0;
+    }
+    if (p1.y < 0) {
+      p1.y = 0;
+    }
+  }
+
+  if (p2 != undefined) {
+    p2.show();
+    p2.update();
+    if (p2.x > window_width) {
+      p2.x = window_width - cell_width;
+    }
+    if (p2.y > window_height) {
+      p2.y = window_height - cell_height;
+    }
+    if (p2.x < 0) {
+      p2.x = 0;
+    }
+    if (p2.y < 0) {
+      p2.y = 0;
+    }
+  }
 }
 
 function renderGrid() {
@@ -47,6 +82,42 @@ function updateAnimation() {
   // TODO
 }
 
-function updateInput() {
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    p1.xSpeed = 0;
+    p1.ySpeed = -1;
+  } else  if (keyCode == DOWN_ARROW) {
+    p1.xSpeed = 0;
+    p1.ySpeed = 1;
+  } else  if (keyCode == LEFT_ARROW) {
+    p1.xSpeed = -1;
+    p1.ySpeed = 0;
+  } else  if (keyCode == RIGHT_ARROW) {
+    p1.xSpeed = 1;
+    p1.ySpeed = 0;
+  } 
+  if (keyCode == 87) {
+    p2.xSpeed = 0;
+    p2.ySpeed = -1;
+  } else  if (keyCode == 83) {
+    p2.xSpeed = 0;
+    p2.ySpeed = 1;
+  } else  if (keyCode == 65) {
+    p2.xSpeed = -1;
+    p2.ySpeed = 0;
+  } else  if (keyCode == 68) {
+    p2.xSpeed = 1;
+    p2.ySpeed = 0;
+  } 
+}
 
+function keyReleased() {
+  if (keyCode == 87 || keyCode == 83 || keyCode == 65 || keyCode == 68) {
+    p2.xSpeed = 0;
+    p2.ySpeed = 0;
+  } else {
+    p1.xSpeed = 0;
+    p1.ySpeed = 0;
+  }
+  return false;
 }
